a6ea3755a34278280018743f005a931adc2599af
pablopunk <yo@pablo.pink>
2020-06-15 01:58:58 +0200

    Allow static export in nextjs

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,52 @@
 import React from 'react'
 import Router from 'next/router'
+import Head from 'next/head'
 import { localeFromBrowser, localeFromRequest } from './locales'
 
 export default (locales: Array<string>) =>
-  class extends React.Component {
+  class extends React.Component<any, { url?: string }> {
     static async getInitialProps({ req, res }) {
-      if (res) {
+      if (res?.writeHead) {
         const locale = localeFromRequest(req, locales)
-        const redirectUrl = '/' + locale
+        const url = '/' + locale
 
-        res.writeHead(302, { Location: redirectUrl })
+        res.writeHead(302, { Location: url })
         res.end()
-      } else {
-        const locale = localeFromBrowser(locales)
-        const redirectUrl = '/' + locale
-
-        Router.push(redirectUrl)
+        return {}
       }
 
       return {}
     }
+
+    componentDidMount() {
+      const locale = localeFromBrowser(locales)
+      const url = '/' + locale
+
+      this.setState({ url })
+    }
+
     render() {
-      return React.createElement('div')
+      if (!this.state?.url) {
+        return <div />
+      }
+
+      const { url } = this.state
+
+      Router.push(url)
+
+      return (
+        <>
+          <Head>
+            {/* Redirects with meta refresh if no JavaScript support */}
+            <noscript>
+              <meta httpEquiv="refresh" content={`0;url=${url}`} />
+            </noscript>
+          </Head>
+          {/* Provides a redirect link if no meta refresh support */}
+          <p>
+            Redirecting to <a href={url}>{url}</a>&hellip;
+          </p>
+        </>
+      )
     }
   }
